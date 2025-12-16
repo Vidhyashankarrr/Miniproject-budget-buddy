18e66319ac8e87d2651c3757700794446950887a
vidhya <vidhyashankar.ril@gmail.com>
2025-12-16 23:51:03 +0530

    first commit-review 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center px-4">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Budget Insights & Expense Tracker - Budget Buddy</h1></div>
+      
+     
+    </>
+  )
+}
+
+export default App
